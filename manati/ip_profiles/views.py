dfcb220abb008da8d51120c181595023622e635c
David Kubesa <d.kubesa@gmail.com>
2018-03-14 10:18:12 +0100

    Added javascript/html part of profiles visualization

--- manati/ip_profiles/views.py
+++ manati/ip_profiles/views.py
@@ -10,12 +10,16 @@ REDIRECT_TO_LOGIN = "/manati_project/login"
 @csrf_exempt
 def index(request):
     context = {}
-    return render(request, 'ip_profiles/index.html', context)
+    #return render(request, 'ip_profiles/index.html', context)
+    return render(request, 'ip_profiles/profile/edit.html', context)
+
 
 @login_required(login_url=REDIRECT_TO_LOGIN)
 @csrf_exempt
 def new(request):
     context = {}
-    return render(request, 'ip_profiles/new.html', context)
+    #return render(request, 'ip_profiles/new.html', context)
+    return render(request, 'ip_profiles/profile/new.html', context)
+
 
 
